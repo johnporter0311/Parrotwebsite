ed0b284bf1b00452c7913904287b0d187fc11c28
johnporter0311 <johnporter0311@gmail.com>
2020-07-01 18:13:59 -0400

    commit

--- lumberjack.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name? ")
-
-if name == "Alyson": 
-    print("{} is a rockstar and she's OK!".format(name))
-else:
-    print("{} sleeps all night and {} works all day!".format(name, name))
