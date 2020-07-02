ffc4ac284ff944baecb7426cbbd5ecee4f66b647
johnporter0311 <johnporter0311@gmail.com>
2020-07-02 17:56:14 -0400

    More add on to Java

--- Scripts/Java.js
+++ Scripts/Java.js
@@ -7,3 +7,23 @@ $(document).ready(function(){
         content: function () { return '<img src="' + $(this).data('img') + '" />'; }
       });  
 });
+
+$(document).ready(function(){
+    $('#destructivePicture').popover(
+      {
+          html: true,
+          trigger: 'click',
+          placement: 'bottom',
+          content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+        });  
+  });
+
+  $(document).ready(function(){
+    $('#pluckingbirdPicture').popover(
+      {
+          html: true,
+          trigger: 'click',
+          placement: 'bottom',
+          content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+        });  
+  });
