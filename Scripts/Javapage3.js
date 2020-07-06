03bdb9c8ed451c81d9a215a8d49c58e6d59dee63
johnporter0311 <johnporter0311@gmail.com>
2020-07-06 19:19:08 -0400

    Add page 2 and 3

---
+++ Scripts/Javapage3.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('#bitePicture').popover(
+      {
+          html: true,
+          trigger: 'click',
+          placement: 'bottom',
+          content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+        });  
+  });
+  
+  $(document).ready(function(){
+      $('#destructivePicture').popover(
+        {
+            html: true,
+            trigger: 'click',
+            placement: 'bottom',
+            content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+          });  
+    });
+  
+    $(document).ready(function(){
+      $('#pluckingbirdPicture').popover(
+        {
+            html: true,
+            trigger: 'click',
+            placement: 'bottom',
+            content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+          });  
+    });
